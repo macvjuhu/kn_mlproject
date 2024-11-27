4eca6e686c4103790f42f88786b340d4eab8930f
Vikas Sharma <vsharma2@cisco.com>
2024-11-27 21:19:45 +0530

    moved common utils, logger ,excption to common module

--- src/common/utils.py
+++ src/common/utils.py
@@ -3,12 +3,11 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-from src.exception import CustomException
+from common.exception import CustomException
 
 def save_object(file_path, obj):
     try:
